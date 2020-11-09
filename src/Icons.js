3df81c0437c9b05198a739b825bdd4a7591c934c
Matheus Oliveira <m.loliveira@alunos.fho.edu.br>
2020-11-09 16:57:49 -0300

    atualizando imgs

--- src/Icons.js
+++ src/Icons.js
@@ -1,13 +1,14 @@
 const Icons = {
-  cvc: require("./icons/stp_card_cvc.png"),
-  cvc_amex: require("./icons/stp_card_cvc_amex.png"),
-  "american-express": require("./icons/stp_card_amex.png"),
-  "diners-club": require("./icons/stp_card_diners.png"),
-  "master-card": require("./icons/stp_card_mastercard.png"),
-  discover: require("./icons/stp_card_discover.png"),
-  jcb: require("./icons/stp_card_jcb.png"),
-  placeholder: require("./icons/stp_card_unknown.png"),
-  visa: require("./icons/stp_card_visa.png"),
+  cvc: require("./icons/cvc.png"),
+  cvc_amex: require("./icons/cvc_amex.png"),
+  "americanexpress": require("./icons/amex.png"),
+  "dinersclub": require("./icons/diners.png"),
+  "mastercard": require("./icons/mastercard.png"),
+  discover: require("./icons/discover.png"),
+  jcb: require("./icons/jcb.png"),
+  placeholder: require("./icons/unknown.png"),
+  visa: require("./icons/visa.png"),
+  elo: require("./icons/elo.png"),
 };
 
 export default Icons;
